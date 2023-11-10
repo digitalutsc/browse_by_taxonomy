f9724b3031273fe0abd11b464dfdc6fe3ae61429
Jeff He <jeffbuyunhe@hotmail.com>
2023-11-10 12:17:42 -0500

    fixed js for views

--- js/browse_by_subject.js
+++ js/browse_by_subject.js
@@ -1,10 +1,13 @@
-/* script for Browse by Subject View */
-jQuery('.view-id-a_z_list_of_subjects .view-content').removeClass("row");
-jQuery('.views-summary').prepend('<li id="show-all-link"><a href="/browse-subjects">All</a></li>');
+(function ($) {
+  'use strict';
+  window.onload = function () {
+    jQuery('.view-header .view-id-a_z_list_of_subjects .view-content ul').prepend('<li id="show-all-link"><a href="/browse-subjects">All</a></li>');
 
-var param = window.location.pathname.split("/").pop();
-if (param == "browse-subjects") {
-  jQuery('#show-all-link').addClass("active");
-} else {
-  jQuery('.is-active').parent().addClass("active");
-}
+    var param = window.location.pathname.split("/").pop();
+    if (param == "browse-subjects") {
+      jQuery('#show-all-link').addClass("active");
+    } else {
+      jQuery('.is-active').parent().addClass("active");
+    }
+  }
+})(jQuery);
